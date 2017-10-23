0a632e27a25b0688d9a74ef293df873184d878a2
kirantr <kirantr@yandex.ru>
2017-10-23 15:04:40 +0300

    book_shop 20.10

---
+++ bookShopVue/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return {
+      url: {
+        home:'http://localhost/sash/book_shop_vue/server/api/',
+        class: 'http://192.168.0.15/~user7/book_shop_vue/client/api/'
+      },
+      location: 'home'
+    }
+  },
+  computed: {
+    getUrl: function(){
+      var self = this
+      if(self.location == 'home'){
+        return self.url.home
+      }
+      else
+      {
+        return self.url.class
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
